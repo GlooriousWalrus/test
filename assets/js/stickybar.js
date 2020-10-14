98888c6eb35e2e560ce5b7d9e1c9476800983693
GlooriousWalrus <glooriouswalrus@protonmail.com>
2020-10-14 22:23:28 +0300

    dotnav fixes

--- assets/js/stickybar.js
+++ assets/js/stickybar.js
@@ -18,21 +18,38 @@ function rmInvert(elem) {
   elem.classList.remove('invert');
 }
 
+var ratiodict = {
+  lipids: 0,
+  cerevisiae: 0,
+  PPP: 0,
+};
+
+// I'm so sorry.
 function intersectionCallback(entries) {
   entries.forEach((entry) => {
     // section 2 inverts nav to white
     if (dict[entry.target.id] === listitems[1]) {
       Array.from(listitems).forEach(
-        entry.isIntersecting ? addInvert : rmInvert
+        entry.isIntersecting && entry.intersectionRatio > 0.4
+          ? addInvert
+          : rmInvert
       );
     }
+    // remove active state
+    dict[entry.target.id].classList.remove('active');
 
-    dict[entry.target.id].classList.toggle('active', entry.isIntersecting);
+    ratiodict[entry.target.id] = entry.intersectionRatio;
   });
+
+  var arr = Object.keys(ratiodict).map(function (key) {
+    return ratiodict[key];
+  });
+  console.log(arr.indexOf(Math.max(...arr)));
+  listitems[arr.indexOf(Math.max(...arr))].classList.add('active');
 }
 
 const observer = new IntersectionObserver(intersectionCallback, {
-  threshold: 0.4,
+  threshold: [0, 0.25, 0.5, 0.75, 1],
 });
 
 observer.observe(section1);
